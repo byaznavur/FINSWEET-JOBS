28a7fdef5bc8fe90cd8fb9a7c7eab8bc8f932b34
azizdev28 <az1z.bakht1yarvich@gmail.com>
2024-12-31 03:45:32 +0500

    process section

--- js/home.js
+++ js/home.js
@@ -1,3 +1,44 @@
+const process = [
+  {
+    id: 1,
+    title: "Discover",
+    image: "../image/home/expert7.png",
+    desc: "We aim to attain the greatest satisfaction for our clients and be one of the prominent.",
+  },
+  {
+    id: 2,
+    title: "Designing",
+    image: "../image/home/expert8.png",
+    desc: "We aim to attain the greatest satisfaction for our clients and be one of the prominent.",
+  },
+  {
+    id: 3,
+    title: "Development",
+    image: "../image/home/expert9.png",
+    desc: "We aim to attain the greatest satisfaction for our clients and be one of the prominent.",
+  },
+  {
+    id: 4,
+    title: "Testing",
+    image: "../image/home/expert10.png",
+    desc: "We aim to attain the greatest satisfaction for our clients and be one of the prominent.",
+  },
+  {
+    id: 5,
+    title: "Deployment",
+    image: "../image/home/expert11.png",
+    desc: "We aim to attain the greatest satisfaction for our clients and be one of the prominent.",
+  },
+  {
+    id: 6,
+    title: "Maintenance",
+    image: "../image/home/expert12.png",
+    desc: "We aim to attain the greatest satisfaction for our clients and be one of the prominent.",
+  },
+];
+
+const processCards = document.querySelector(".process-wrapper-cards");
+
 const solutionsWrapperCards = document.querySelector(
   ".solutions-wrapper-cards"
 );
@@ -31,7 +72,7 @@ let users;
 function getUserCards(user) {
   console.log(user);
 
-  let randomId = Math.floor(Math.random() * 3) + 1;
+  let randomId = Math.floor(Math.random() * 12) + 1;
   return `
     <div class="solutions-wrapper-cards-item">
   <div class = "herro-card">
@@ -57,4 +98,23 @@ getData("https://jsonplaceholder.typicode.com/users").then((res) => {
   users.map((user) => {
     solutionsWrapperCards.innerHTML += getUserCards(user);
   });
+});
+
+function getProcess(el) {
+  return `
+     <div class = "process-wrapper-cards-item">
+            <span>0${el.id}</span>
+            <img src=${el.image} alt=${el.title} />
+            <div>
+              <h3>${el.title}</h3>
+              <p>${el.desc}</p>
+            </div>
+
+            <img class="process-line" src = "../image/home/gorizantal.png"/>
+          </div>
+  `;
+}
+
+process.map((el) => {
+  processCards.innerHTML += getProcess(el);
 });
